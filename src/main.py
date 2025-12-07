98b2daf35b6b18584bb496f4768ebc3aa56c2ebc
Alexander Andersson <alexander.andersson.pl@gmail.com>
2025-12-07 10:10:01 +0100

    remove experimental

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from rules.static import bank_account, name, credit_card, email, pesel_rule, age
 import morfeusz2
 from flask import Flask, request, jsonify
-from rules.ner import gliner
+
 
 def mapToString(x) -> str:
     if isinstance(x, str):
@@ -43,8 +43,7 @@ def anonymize(input: str, regenerate: bool) -> str:
         else:
             parsed.append(" ".join(map(regenerateToken, tokens)))
 
-    out = ". ".join(parsed)
-    return gliner.anonymize(out)
+    return ". ".join(parsed)
 
 
 @app.route('/api/parse', methods=['POST'])
